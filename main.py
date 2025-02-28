08cf2e5f182e760963263c87f01281d327391088
Tim6400 <124497065+Tim6400@users.noreply.github.com>
2025-02-28 11:37:04 +0100

    Système de configuration mit en place

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import json
 import subprocess
 import os
 
+Infos_dev = {
+    "Version": "0.0.2",
+    "Contributeurs" : 0,
+    "maj" : "28/02/25"
+}
+
 def create_backup():
-    print('''
-------- 📌 CONNEXION DB -------''')
-    connect_db = {
-        "host": input("🔹 Host du serveur PostgreSQL : "),
-        "database": input("🔹 Nom db PostgreSQL : "),
-        "user": input("🔹 Nom d'utilisateur PostgreSQL : "),
-        "password": input("🔹 Mot de passe PostgreSQL : "),
-        "port": input("🔹 Port PostgreSQL : ")
-    }
+    with open("data.json", "r", encoding="utf-8") as fichier:
+        connect_db = json.load(fichier)
 
     os.environ["PGPASSWORD"] = connect_db["password"]  
 
@@ -53,12 +52,108 @@ def menu() :
     elif option == "3":
         dev()
     elif option == "4":
-        dev()
+        config()
     else :
         print("❌ Option non valide")
 
-def start() :
+def connect_db(modif=False):
+    if not modif:
+        print('''
+------- 📌 CONNEXION DB -------''')
+        db_infos = {
+            "host": input("🔹 Host du serveur PostgreSQL : "),
+            "database": input("🔹 Nom db PostgreSQL : "),
+            "user": input("🔹 Nom d'utilisateur PostgreSQL : "),
+            "password": input("🔹 Mot de passe PostgreSQL : "),
+            "port": input("🔹 Port PostgreSQL : ")
+        }
+        with open("data.json", "w", encoding="utf-8") as fichier:
+            json.dump(db_infos, fichier, indent=4, ensure_ascii=False)
+        print("✅ Infos enregistrées")
+        return
+    
     print('''
+------- 🖋️ MODIFICATION DB -------
+
+Info à modifier :
+1️⃣  Host du serveur PostgreSQL
+2️⃣  Nom db PostgreSQL
+3️⃣  Nom d'utilisateur PostgreSQL 
+4️⃣  Mot de passe PostgreSQL
+5️⃣  Port PostgreSQL
+
+Autres options :
+6️⃣  Tout modifier
+7️⃣  Retour au menu
+''')
+
+    try:
+        option = int(input("🔹 Choisissez une option (1 à 7) : "))
+    except ValueError:
+        print("❌ Entrée invalide, veuillez entrer un nombre.")
+        connect_db(modif)
+        return
+
+    try:
+        with open("data.json", "r", encoding="utf-8") as fichier:
+            db_infos = json.load(fichier)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("⚠️ Le fichier de configuration est introuvable ou corrompu. Création d'un nouveau fichier...")
+        connect_db()
+        return
+
+    if "host" not in db_infos and option != 7:
+        connect_db()
+        return
+
+    match option:
+        case 1:
+            db_infos["host"] = input("🔹 Nouveau host du serveur PostgreSQL : ")
+        case 2:
+            db_infos["database"] = input("🔹 Nouveau nom db PostgreSQL : ")
+        case 3:
+            db_infos["user"] = input("🔹 Nouveau nom d'utilisateur PostgreSQL : ")
+        case 4:
+            db_infos["password"] = input("🔹 Nouveau mot de passe PostgreSQL : ")
+        case 5:
+            db_infos["port"] = input("🔹 Nouveau port PostgreSQL : ")
+        case 6:
+            connect_db()
+            return
+        case 7:
+            menu()
+            return
+        case _:
+            print("❌ Option non valide")
+            connect_db(modif)
+            return
+
+    with open("data.json", "w", encoding="utf-8") as fichier:
+        json.dump(db_infos, fichier, indent=4, ensure_ascii=False)
+    print("✅ Infos modifiées")
+    menu()
+        
+def config() :
+    print('''
+---------  🔧CONFIGURATION   ---------
+1️⃣ Changer les identifients de la bdd
+2️⃣ Changer le repository
+3️⃣ Retour au menu
+          ''')
+    optionBrut = input("🔹 Choisissez une option (1, 2 ou 3) : ")
+    option = int(optionBrut)
+    
+    if option == 1 :
+        connect_db(True)
+    elif option == 2 :
+        dev()
+    elif option == 3 :
+        menu()
+    else : 
+        print("❌ Option non valide")
+
+def start() :
+    print(f'''
             ████████╗██╗██████╗░
             ╚══██╔══╝██║██╔══██╗
             ░░░██║░░░██║██████╦╝
@@ -68,10 +163,10 @@ def start() :
         Transfer, Integrate, Backup
 
 ===============================================
-=  Versiion : 0.0.1                           =
+=  Version : {Infos_dev["Version"]}                            =
 =  Créateur : TML (Timéo Menvielle Larrouy)   =
-=  Contributeurs : 0                          =
-=  Mise à jour : 27/02/2025                   =
+=  Contributeurs : {Infos_dev["Contributeurs"]}                          =
+=  Mise à jour : {Infos_dev["maj"]}                     =
 ===============================================
 
 
